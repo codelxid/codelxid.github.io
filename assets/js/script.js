b455910146f272044f959cd93ae4357778cd159d
Frits Sasia <fritssasia@gmail.com>
2020-11-18 16:49:22 +0800

    menghapus fungsi mengubah warna bg .navbar saat scroll

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// Navbar
-$(window).on("scroll", function () {
-    if ($(window).scrollTop() > 50) {
-        $(".header").addClass("nav-active bg-light");
-    } else {
-        $(".header").removeClass("nav-active");
-    }
-});
